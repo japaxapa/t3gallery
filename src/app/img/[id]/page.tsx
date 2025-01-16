ceba630591a7cc97585c30d62bd5b38cbcebecf8
Mario Kawakita <mario.jhk@gmail.com>
2025-01-16 13:46:30 -0300

    dialog full screen | component for image | check modal

--- src/app/img/[id]/page.tsx
+++ src/app/img/[id]/page.tsx
@@ -1,8 +1,14 @@
-export default async function PhotoModal({
+import FullPageImageView from "~/components/full-image-page";
+
+export default async function PhotoPage({
   params,
 }: {
   params: { id: string };
 }) {
   const { id: photoId } = await params;
-  return <div>{photoId}</div>;
+
+  const idAsNumber = Number(photoId);
+  if (Number.isNaN(idAsNumber)) throw new Error("Invalid photo id");
+
+  return <FullPageImageView id={idAsNumber} />;
 }
