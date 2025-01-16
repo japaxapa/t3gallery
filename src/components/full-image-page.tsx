eab29befeed42dea131534d105dc6758e2a12d63
Mario Kawakita <mario.jhk@gmail.com>
2025-01-16 14:07:05 -0300

    more cleanup on modal | check modal

--- src/components/full-image-page.tsx
+++ src/components/full-image-page.tsx
@@ -2,5 +2,13 @@ import { getImage } from "~/server/queries";
 
 export default async function FullPageImageView(props: { id: number }) {
   const image = await getImage(props.id);
-  return <img src={image.url} className="w-96" />;
+  return (
+    <div className="flex h-full w-full min-w-0">
+      <div className="flex-shrink flex justify-center items-center"></div>
+      <img src={image.url} className="w-96 flex-shrink object-contain" />;
+      <div className="flex w-48 flex-shrink-0 flex-col border-l">
+        <div className="text-xl font-bold">{image.name}</div>
+      </div>
+    </div>
+  );
 }
