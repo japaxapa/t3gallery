2e8393340618f957652ba86445f7bf3dae9e097f
Mario Kawakita <mario.jhk@gmail.com>
2025-01-18 18:25:09 -0300

    Analytics on delete button

--- src/server/queries.ts
+++ src/server/queries.ts
@@ -5,6 +5,7 @@ import { images } from "./db/schema";
 import { and, eq } from "drizzle-orm";
 import { redirect } from "next/navigation";
 import { revalidatePath } from "next/cache";
+import analyticsServerClient from "./analytics";
 
 export async function getMyImages() {
   const user = await auth();
@@ -44,6 +45,14 @@ export async function deleteImage(id: number) {
   await db
     .delete(images)
     .where(and(eq(images.id, id), eq(images.userId, user.userId)));
+
+  analyticsServerClient.capture({
+    distinctId: user.userId,
+    event: "delete image",
+    properties: {
+      imageId: id,
+    },
+  });
 
   revalidatePath("/ ");
   redirect("/ ");
